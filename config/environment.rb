d68f71bae774fa9a79b4af20cb62143e814153ae
pawel <pawel@pawel-laptop.pl>
2010-01-18 22:50:53 +0100

    added .gem file

--- config/environment.rb
+++ config/environment.rb
@@ -73,6 +73,8 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
   
+  config.gem 'mislav-will_paginate' , :version => '~> 2.3.2' ,
+      :lib => 'will_paginate' , :source => 'http://gems.github.com'
 end
 
 ActiveRecord::Base.pluralize_table_names = false
