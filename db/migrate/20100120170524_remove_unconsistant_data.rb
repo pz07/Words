61f70da3519fbf38a77cc4f38f73cc620adaf037
pawel <pawel@pawel-laptop.pl>
2010-01-20 23:15:06 +0100

    dalsze ulepszenia procesu nauki

--- db/migrate/20100120170524_remove_unconsistant_data.rb
+++ db/migrate/20100120170524_remove_unconsistant_data.rb
@@ -3,7 +3,7 @@ class RemoveUnconsistantData < ActiveRecord::Migration
     Level.find(:all, :conditions => "learning_schema_id is null").each do |l|
       l.destroy!
     end
-    Question.find(:all, :conditions => "lesson_id is null").each do |q|
+    Question.find(:all, :conditions => "lesson_id is null or level_id is null").each do |q|
       q.destroy!
     end
     Answer.find(:all, :conditions => "question_id is null").each do |a|
