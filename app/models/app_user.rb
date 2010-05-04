fac01b97b9b69f47937689e48bc4ea8441039fee
Paweł Zięba <pawel.zieba83@gmail.com>
2010-05-04 12:24:35 +0200

    SuperMemo method implemented.

--- app/models/app_user.rb
+++
@@ -1,8 +0,0 @@
-class AppUser < ActiveRecord::Base
-    set_table_name "app_user"
-  
-    acts_as_authentic
-    #acts_as_authentic do |c|
-    #  c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
-    #end # block optional
-end
--- app/models/question_level_stat.rb
+++
@@ -1,9 +0,0 @@
-class QuestionLevelStat < ActiveRecord::Base
-
-  #zależności
-  belongs_to :question
-  belongs_to :level
-  
-  #walidacja
-  validates_presence_of :question, :level, :learning_begin, :correct_answers, :wrong_answers
-end
